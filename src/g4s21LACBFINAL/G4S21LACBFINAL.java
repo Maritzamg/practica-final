2fecd19e6e5fbca5d690c53734018d42bb0ebca0
tanne <tanne@LAPTOP-F2TF4AUH>
2022-06-27 18:06:07 -0600

    LUIS ALBERTO CRUZ BARAHONA

---
+++ src/g4s21LACBFINAL/G4S21LACBFINAL.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21LACBFINAL;
+
+/**
+ *
+ * @author Asus
+ */
+public class G4S21LACBFINAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    new inter ().setVisible(true);
+    
+    }
+    
+    
+}
